96279b8c51ca2b7fe13e5cedbf9f54cf62ecfcbe
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2023-11-06 23:45:56 -0800

    comments and clean for addChatGPTresponse.js

--- addChatGPTresponse.js
+++ addChatGPTresponse.js
@@ -1,16 +1,11 @@
 import { config } from 'dotenv';
-
-// Load environment variables from the .env file
-config();
-
+config(); // Load environment variables from the .env file
 import OpenAI from "openai";
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY}) // Initialize openai object
 
 export const addChatGPTresponse = async (business, messages) => {
-    console.log("1")
-    console.log(business)
-    console.log(messages)
-    console.log("1")
+
+    // Give AI some context
     if (messages.length == 0) {
         messages = [
             {"role": "system", "content": "You are a helpful yelp reviews assistant. You help people find provided information in yelp reviews. It's really important that you are concise and clear."},
@@ -19,16 +14,14 @@ export const addChatGPTresponse = async (business, messages) => {
         ]
     }
 
+    // The response
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages,
     })
 
+    // Add the chat message from ChatGPT to the existing array of messages
     messages = [...messages, response.choices[0].message]
-
     console.log(`response.choices[0].message`, response.choices[0].message)
-
     return messages
 }
-
-// module.exports = { addChatGPTresponse }
