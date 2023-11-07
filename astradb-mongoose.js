5bc6c126768386e3a3f5426c3a1d83312e67742c
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2023-11-06 23:55:30 -0800

    astradb-mongoose.js clean

--- astradb-mongoose.js
+++ astradb-mongoose.js
@@ -1,28 +1,7 @@
 import { config } from 'dotenv';
-
-// Load environment variables from the .env file
 config();
-
 import mongoose from 'mongoose';
-// const mongoose = require("mongoose");
 import { driver, createAstraUri } from "stargate-mongoose"
-
-// export const connectToAstraDb = async () => {
-//     const uri = createAstraUri(
-//         process.env.ASTRA_DB_ID,
-//         process.env.ASTRA_DB_REGION,
-//         process.env.ASTRA_DB_KEYSPACE,
-//         process.env.ASTRA_DB_APPLICATION_TOKEN,
-//     )
-
-//     mongoose.set("autoCreate", true);
-//     mongoose.setDriver(driver);
-
-//     await mongoose.connect(uri, {
-//         isAstra: true,
-//     });
-// 	console.log("Complete")
-// }
 
 export const connectToAstraDb = async () => {
     try {
@@ -46,7 +25,6 @@ export const connectToAstraDb = async () => {
 }
 
 export const initMongooseBusinessModel = async () => {
-	// await mongoose.connection.dropCollection("videos");
 	const Business = mongoose.model(
 	  "Business",
 	  new mongoose.Schema(
@@ -72,29 +50,3 @@ export const initMongooseBusinessModel = async () => {
 	await Business.init();
 	console.log("complete")
 };
-
-// const businessSchema = new mongoose.Schema(
-// 	{
-// 		business_id: String,
-// 		url: String,
-// 		reviews: String,
-// 		$vector: {
-// 		type: [Number],
-// 		validate: (vector) => vector && vector.length === 1536,
-// 	  },
-// 	},
-// 	{
-// 	  collectionOptions: {
-// 		vector: {
-// 		  size: 1536,
-// 		  function: "cosine",
-// 		},
-// 	  },
-// 	},
-// );
-// const Business = mongoose.model("Business", businessSchema);
-// export default Business; 
-
-// console.log(await initMongooseBusinessModel())
-
-// module.exports = { connectToAstraDb, initMongooseBusinessModel };
