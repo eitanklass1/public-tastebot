53474a42454b722cfb6de59016c8d88ac9288bfb
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2023-11-07 00:05:23 -0800

    comment and clean for getYelpReviews.js

--- getYelpReviews.js
+++ getYelpReviews.js
@@ -1,17 +1,15 @@
 import { config } from 'dotenv';
 config()
-
 import axios from 'axios';
 
+// For this code to work this EXACT format is needed for the yelp web address: "https://www.yelp.com/biz/steam-palo-alto" -- this should be changed in the futures to be more versatile
 export const getBusinessIdFromUrl = (url) => {
     console.log(`getBusinessIdFromUrl: ${url}`)
     let businessId = url.split('biz/')[1]
     return businessId
 }
 
-// console.log(getBusinessIdFromUrl('https://www.yelp.com/biz/smack-burgers-san-jose'))
-// https://www.yelp.com/biz/steam-palo-alto -- THIS NEEDS TO BE THE FORMAT
-
+// The Yelp API only gets 3 reviews and truncates each review to 40 chars -- this is a potential function to work on in the future to make it get 10+ FULL reviews
 const getYelpReviewsJSON = async (url) => {
 
     const businessId = getBusinessIdFromUrl(url)
@@ -43,15 +41,9 @@ const getYelpReviewsString = (jsonYelpReviews) => {
     return stringYelpReviews
 }
 
+// This returns the string of the reviews and uses "export" for it to be called outside of this file
 export const getYelpReviews = async (url) => {
     const reviewsJSON = await getYelpReviewsJSON(url)
     const reviewsString = await getYelpReviewsString(reviewsJSON)
     return reviewsString
 }
-
-// let test = await getYelpReviewsJSON("https://www.yelp.com/biz/steam-palo-alto")
-// console.log(getYelpReviewsString(test))
-// console.log(test)
-
-// console.log(await getYelpReviews("https://www.yelp.com/biz/smack-burgers-san-jose"))
-// console.log(await getYelpReviewsJSON("https://www.yelp.com/biz/steam-palo-alto"))
